831d69ebef450afa8a47ac8889c1bd74ba0d728e
duyoung jang <endud5118@gmail.com>
2024-11-24 22:41:45 +1300

    add104

--- 14longestCommmonPrefix.py
+++ 14longestCommmonPrefix.py
@@ -5,7 +5,9 @@ class Solution:
         
         for i in range(len(strs[0])):
             c = strs[0][i] 
+
             for j in range(1, len(strs)):
                 if i == len(strs[j]) or strs[j][i] != c:
                     return strs[0][0:i]
+                
         return strs[0]
